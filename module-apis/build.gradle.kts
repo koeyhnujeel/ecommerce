e9889ccc189ac9319d2950eb3d83cb7610e1e4fd
zunza <fksfks015@naver.com>
2025-11-03 23:34:20 +0900

    ​

---
+++ module-apis/build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+    implementation(project(":module-domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt:0.12.6") // JWT
+}
+
+tasks.named("bootJar") {
+    enabled = false
+}
+
+tasks.named("jar") {
+    enabled = true
+}
---
+++ module-apis/customer-api/build.gradle.kts
@@ -0,0 +1,27 @@
+dependencies {
+    implementation(project(":module-apis"))
+    implementation(project(":module-domain"))
+    implementation(project(":module-infra"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt:0.12.6") // JWT
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-testcontainers:3.5.7") // Testcontainers
+    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+    testImplementation("org.testcontainers:postgresql:1.21.3")
+    testImplementation("com.redis:testcontainers-redis:2.2.4")
+}
+
+tasks.named("bootJar") {
+    enabled = false
+}
+
+tasks.named("jar") {
+    enabled = true
+}
