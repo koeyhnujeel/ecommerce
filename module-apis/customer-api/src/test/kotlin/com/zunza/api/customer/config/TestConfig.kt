a066ecdf195e3b593f7d1882b08fe8856c564426
zunza <fksfks015@naver.com>
2025-11-03 23:31:24 +0900

    feat: 테스트 설정 추가

---
+++ module-apis/customer-api/src/test/kotlin/com/zunza/api/customer/config/TestConfig.kt
@@ -0,0 +1,10 @@
+package com.zunza.api.customer.config
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
+import io.kotest.core.spec.IsolationMode
+import io.kotest.extensions.spring.SpringExtension
+
+class TestConfig : AbstractProjectConfig() {
+    override fun extensions(): List<Extension> = listOf(SpringExtension)
+}
