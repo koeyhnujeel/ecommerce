f41ff2b386dbcd9cf06f12b7e8f1f0961be70722
zunza <fksfks015@naver.com>
2025-11-03 23:17:41 +0900

    feat: JWT 인증 구현

---
+++ module-apis/src/main/kotlin/com/zunza/apis/auth/jwt/handler/JwtAccessDeniedHandler.kt
@@ -0,0 +1,31 @@
+package com.zunza.apis.auth.jwt.handler
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.zunza.apis.support.exception.ErrorResponse
+import com.zunza.apis.support.resopnse.ApiResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+import java.nio.charset.StandardCharsets
+
+class JwtAccessDeniedHandler(
+    private val objectMapper: ObjectMapper,
+) : AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        accessDeniedException: AccessDeniedException?,
+    ) {
+        response!!.status = HttpStatus.FORBIDDEN.value()
+        response.contentType = MediaType.APPLICATION_JSON.toString()
+        response.characterEncoding = StandardCharsets.UTF_8.name()
+
+        val errorResponse = ErrorResponse(message = "접근 권한이 없습니다.")
+        val apiResponse = ApiResponse.error(errorResponse)
+
+        objectMapper.writeValue(response.writer, apiResponse)
+    }
+}
