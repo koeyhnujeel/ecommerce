bac061f2c6b199084f9c72129afcacfe6de86dee
zunza <fksfks015@naver.com>
2025-11-12 19:49:28 +0900

    feat: 상품 카테고리 엔티티 및 레포지토리 추가

---
+++ module-domain/src/main/kotlin/com/zunza/domain/repository/ProductCategoryRepository.kt
@@ -0,0 +1,8 @@
+package com.zunza.domain.repository
+
+import com.zunza.domain.entity.ProductCategory
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductCategoryRepository : JpaRepository<ProductCategory, Long>
