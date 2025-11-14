c2b699d99da0adaa680f16040b167d88bc5f81be
zunza <fksfks015@naver.com>
2025-11-14 11:58:58 +0900

    fix: implements JpaRepository

--- module-domain/src/main/kotlin/com/zunza/domain/repository/ProductImageRepository.kt
+++ module-domain/src/main/kotlin/com/zunza/domain/repository/ProductImageRepository.kt
@@ -1,6 +1,8 @@
 package com.zunza.domain.repository
 
+import com.zunza.domain.entity.ProductImage
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProductImageRepository
+interface ProductImageRepository : JpaRepository<ProductImage, Long>
--- module-domain/src/main/kotlin/com/zunza/domain/repository/ProductOptionRepository.kt
+++ module-domain/src/main/kotlin/com/zunza/domain/repository/ProductOptionRepository.kt
@@ -1,6 +1,8 @@
 package com.zunza.domain.repository
 
+import com.zunza.domain.entity.ProductOption
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProductOptionRepository
+interface ProductOptionRepository : JpaRepository<ProductOption, Long>
