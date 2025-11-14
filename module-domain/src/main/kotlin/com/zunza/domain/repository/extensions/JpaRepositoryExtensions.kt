1fd36726c512de1fe39fa951eee8cb20667790ac
zunza <fksfks015@naver.com>
2025-11-14 12:13:58 +0900

    ​

--- module-domain/src/main/kotlin/com/zunza/domain/repository/extensions/JpaRepositoryExtensions.kt
+++ module-domain/src/main/kotlin/com/zunza/domain/repository/extensions/JpaRepositoryExtensions.kt
@@ -6,8 +6,7 @@ import org.springframework.data.repository.findByIdOrNull
 
 inline fun <reified T, ID : Any> JpaRepository<T, ID>.findByIdOrThrow(
     id: ID,
-    entityName: String = T::class.java.simpleName
-): T {
-    return this.findByIdOrNull(id)
+    entityName: String = T::class.java.simpleName,
+): T =
+    this.findByIdOrNull(id)
         ?: throw ErrorCode.NOT_FOUND.exception("$entityName 정보를 찾을 수 없습니다.")
-}
