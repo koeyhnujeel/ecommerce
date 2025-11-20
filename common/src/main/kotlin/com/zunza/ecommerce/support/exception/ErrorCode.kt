75eb42c59fae676d08baa247a59793ad93b686e5
zunza <fksfks015@naver.com>
2025-11-21 00:10:07 +0900

    feat: 에러코드 추가

--- common/src/main/kotlin/com/zunza/ecommerce/support/exception/ErrorCode.kt
+++ common/src/main/kotlin/com/zunza/ecommerce/support/exception/ErrorCode.kt
@@ -4,6 +4,9 @@ enum class ErrorCode(
     val status: Int,
     val defaultMessage: String,
 ) {
+    EMAIL_ALREADY_EXISTS(409, "이미 사용 중인 이메일입니다."),
+    PHONE_ALREADY_EXISTS(409, "이미 사용 중인 번호입니다."),
+    CUSTOMER_NOT_FOUND(404, "고객 정보를 찾을 수 없습니다."),
     ;
 
     fun exception() = BusinessException(this, this.defaultMessage)
