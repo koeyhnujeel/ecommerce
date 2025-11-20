f588d07e37eb4f1b9650a1a9a78254ce310c2ee8
zunza <fksfks015@naver.com>
2025-11-21 00:12:20 +0900

    feat: 비밀번호 암호화(BCrypt) 추가

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("at.favre.lib:bcrypt:0.10.2")
 
     runtimeOnly("org.postgresql:postgresql")
 }
---
+++ infra/src/main/kotlin/com/zunza/ecommerce/adapter/BCryptPasswordEncoder.kt
@@ -0,0 +1,20 @@
+package com.zunza.ecommerce.adapter
+
+import at.favre.lib.crypto.bcrypt.BCrypt
+import com.zunza.ecommerce.port.PasswordEncoder
+import org.springframework.stereotype.Component
+
+@Component
+class BCryptPasswordEncoder(
+) : PasswordEncoder {
+    private val bcrypt = BCrypt.withDefaults()
+    private val verifier = BCrypt.verifyer()
+
+    override fun encode(rawPassword: String): String {
+        return bcrypt.hashToString(12, rawPassword.toCharArray())
+    }
+
+    override fun matches(rawPassword: String, encodedPassword: String): Boolean {
+        return verifier.verify(rawPassword.toCharArray(), encodedPassword).verified
+    }
+}
