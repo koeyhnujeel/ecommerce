da977ea2f288bc267ecf915dd8648eae949a290a
zunza <fksfks015@naver.com>
2025-11-20 23:57:15 +0900

    feat: 회원가입 로직 구현 - PasswordEncoder(비밀번호 암호화) - NicknameGenerator(랜덤 닉네임 생성)

---
+++ domain/src/main/kotlin/com/zunza/ecommerce/service/AuthService.kt
@@ -0,0 +1,46 @@
+package com.zunza.ecommerce.service
+
+import com.zunza.ecommerce.domain.Customer
+import com.zunza.ecommerce.dto.SignupCommand
+import com.zunza.ecommerce.port.PasswordEncoder
+import com.zunza.ecommerce.repository.CustomerRepository
+import com.zunza.ecommerce.support.exception.ErrorCode
+import com.zunza.ecommerce.util.NicknameGenerator
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService(
+    private val customerRepository: CustomerRepository,
+    private val passwordEncoder: PasswordEncoder
+) {
+    fun validateEmailAvailable(email: String) {
+        if (customerRepository.existsByEmail(email)) {
+            throw ErrorCode.EMAIL_ALREADY_EXISTS.exception()
+        }
+    }
+
+    fun validatePhoneAvailable(phone: String) {
+        if (customerRepository.existsByPhone(phone)) {
+            throw ErrorCode.PHONE_ALREADY_EXISTS.exception()
+        }
+    }
+
+    fun createCustomer(command: SignupCommand) {
+        val encodedPassword = passwordEncoder.encode(command.password)
+        val nickname = getRandomNickname()
+
+        val customer = Customer.of(
+            command.email,
+            encodedPassword,
+            command.name,
+            nickname,
+            command.phone
+        )
+
+        customerRepository.save(customer)
+    }
+
+    private fun getRandomNickname(): String =
+        generateSequence { NicknameGenerator.generate() }
+            .first { !customerRepository.existsByNickname(it) }
+}
