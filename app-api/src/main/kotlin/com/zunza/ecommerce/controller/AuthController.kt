15a4da45dedf260c929c6754b2ed531942b9130f
zunza <fksfks015@naver.com>
2025-11-20 23:52:52 +0900

    feat: 회원가입 컨트롤러 구현 - 이메일 중복 체크 - 핸드폰 번호 중복 체크 - 고객 회원가입

---
+++ app-api/src/main/kotlin/com/zunza/ecommerce/controller/AuthController.kt
@@ -0,0 +1,45 @@
+package com.zunza.ecommerce.controller
+
+import com.zunza.ecommerce.dto.SignupRequest
+import com.zunza.ecommerce.service.AuthService
+import com.zunza.ecommerce.support.resopnse.ApiResponse
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/auth")
+class AuthController(
+    private val authService: AuthService,
+) {
+    @GetMapping("/email/validation")
+    fun checkEmailDuplicate(
+        @RequestParam email: String
+    ): ApiResponse<Any> {
+        authService.validateEmailAvailable(email)
+        return ApiResponse.success()
+    }
+
+    @GetMapping("/phone/validation")
+    fun checkPhoneDuplicate(
+        @RequestParam phone: String
+    ): ApiResponse<Any> {
+        authService.validatePhoneAvailable(phone)
+        return ApiResponse.success()
+    }
+
+    @PostMapping("/signup/customer")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun signupCustomer(
+        @Valid @RequestBody request: SignupRequest,
+    ): ApiResponse<Any> {
+        authService.createCustomer(request.toCommand())
+        return ApiResponse.success()
+    }
+}
