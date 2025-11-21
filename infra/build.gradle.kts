84fbf74f06e204045300888731f589c121e97546
zunza <fksfks015@naver.com>
2025-11-21 22:02:06 +0900

    faet: JWT 의존성 추가

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -5,6 +5,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("at.favre.lib:bcrypt:0.10.2")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6") // JWT
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     runtimeOnly("org.postgresql:postgresql")
 }
