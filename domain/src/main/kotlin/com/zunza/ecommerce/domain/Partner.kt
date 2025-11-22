488994dca3c0575a44c8a967129bcc0b2dcb1269
zunza <fksfks015@naver.com>
2025-11-22 11:37:41 +0900

    feat: 프로퍼티 추가

--- domain/src/main/kotlin/com/zunza/ecommerce/domain/Partner.kt
+++ domain/src/main/kotlin/com/zunza/ecommerce/domain/Partner.kt
@@ -1,9 +1,13 @@
 package com.zunza.ecommerce.domain
 
 import com.zunza.ecommerce.domain.enums.PartnerStatus
+import com.zunza.ecommerce.domain.enums.UserType
+import java.time.LocalDateTime
 
 class Partner(
     val id: Long,
+    val email: String,
+    val password: String,
     val representativeName: String,
     val businessName: String,
     val businessNumber: String,
@@ -11,4 +15,7 @@ class Partner(
     val settlementAccountNumber: String,
     val phone: String,
     val status: PartnerStatus,
+    val userType: UserType,
+    val createdAt: LocalDateTime? = null,
+    val updatedAt: LocalDateTime? = null,
 )
