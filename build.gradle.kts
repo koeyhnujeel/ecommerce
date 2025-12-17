368e335558d76b3b9dfa751f176e7e8c3c980ab8
koeyhnujeel <fksfks015@naver.com>
2025-12-17 15:56:33 +0900

    ​

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.5.8" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+allprojects {
+    group = "com.zunza"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "java")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
+    }
+
+    dependencies {
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1") // Kotest Assertions
+        testImplementation("io.mockk:mockk:1.14.2") // Mockk
+        testImplementation("com.ninja-squad:springmockk:4.0.2")
+
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "ecommerce"
+
+include(
+    "api",
+    "core",
+)
