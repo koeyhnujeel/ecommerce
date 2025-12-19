ce766836f2dc82e63b389426fa754075d24eae1f
koeyhnujeel <fksfks015@naver.com>
2025-12-19 12:53:07 +0900

    feat: Spring Security, JWT 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,7 +4,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.security:spring-security-core") // 임시
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers:3.5.7") // Testcontainers
@@ -15,6 +16,8 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.named("bootJar") {
