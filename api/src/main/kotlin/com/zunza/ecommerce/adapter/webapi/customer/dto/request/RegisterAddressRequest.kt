a23dcbc22264a68ec48d57a05b8681b593866644
koeyhnujeel <fksfks015@naver.com>
2025-12-29 19:03:34 +0900

    feat: 배송지 등록, 수정, 삭제, 기본 배송지 업데이트 컨트롤러 구현

---
+++ api/src/main/kotlin/com/zunza/ecommerce/adapter/webapi/customer/dto/request/RegisterAddressRequest.kt
@@ -0,0 +1,28 @@
+package com.zunza.ecommerce.adapter.webapi.customer.dto.request
+
+import com.zunza.ecommerce.application.customer.service.dto.command.RegisterAddressCommand
+import jakarta.validation.constraints.NotBlank
+
+data class RegisterAddressRequest(
+    @field:NotBlank(message = "배송지 별칭을 입력해 주세요.")
+    val alias: String,
+    @field:NotBlank(message = "주소를 입력해 주세요.")
+    val roadAddress: String,
+    val detailAddress: String,
+    @field:NotBlank(message = "수령인을 입력해 주세요.")
+    val receiverName: String,
+    @field:NotBlank(message = "우편번호를 입력해 주세요.")
+    val zipcode: String,
+    val isDefault: Boolean
+) {
+    fun toCommand(accountId: Long) =
+        RegisterAddressCommand(
+            accountId = accountId,
+            alias = alias,
+            roadAddress = roadAddress,
+            detailAddress = detailAddress,
+            receiverName = receiverName,
+            zipcode = zipcode,
+            isDefault = isDefault
+        )
+}
---
+++ api/src/main/kotlin/com/zunza/ecommerce/adapter/webapi/customer/dto/request/UpdateAddressRequest.kt
@@ -0,0 +1,29 @@
+package com.zunza.ecommerce.adapter.webapi.customer.dto.request
+
+import com.zunza.ecommerce.application.customer.service.dto.command.UpdateAddressCommand
+import jakarta.validation.constraints.NotBlank
+
+data class UpdateAddressRequest(
+    @field:NotBlank(message = "배송지 별칭을 입력해 주세요.")
+    val alias: String,
+    @field:NotBlank(message = "주소를 입력해 주세요.")
+    val roadAddress: String,
+    val detailAddress: String,
+    @field:NotBlank(message = "수령인을 입력해 주세요.")
+    val receiverName: String,
+    @field:NotBlank(message = "우편번호를 입력해 주세요.")
+    val zipcode: String,
+    val isDefault: Boolean
+) {
+    fun toCommand(accountId: Long, addressId: Long) =
+        UpdateAddressCommand(
+            accountId = accountId,
+            addressId = addressId,
+            alias = alias,
+            roadAddress = roadAddress,
+            detailAddress = detailAddress,
+            receiverName = receiverName,
+            zipcode = zipcode,
+            isDefault = isDefault
+        )
+}
