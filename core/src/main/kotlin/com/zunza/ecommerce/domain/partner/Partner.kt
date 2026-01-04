5ddeadb06ab1d13871a97ba412b0cd6767df3320
koeyhnujeel <fksfks015@naver.com>
2026-01-04 14:12:23 +0900

    feat: Partner(판매자) 엔티티 추가

---
+++ core/src/main/kotlin/com/zunza/ecommerce/domain/partner/Partner.kt
@@ -0,0 +1,27 @@
+package com.zunza.ecommerce.domain.partner
+
+import com.zunza.ecommerce.domain.AbstractEntity
+import com.zunza.ecommerce.domain.shared.BankAccount
+import jakarta.persistence.Entity
+
+@Entity
+class Partner(
+    val accountId: Long,
+    val partnerApplicationId: Long,
+    val businessInfo: BusinessInfo,
+    val settlementAccount: BankAccount,
+) : AbstractEntity() {
+    companion object {
+        fun register(
+            accountId: Long,
+            partnerApplicationId: Long,
+            businessInfo: BusinessInfo,
+            settlementAccount: BankAccount,
+        ) = Partner(
+            accountId = accountId,
+            partnerApplicationId = partnerApplicationId,
+            businessInfo = businessInfo,
+            settlementAccount = settlementAccount
+        )
+    }
+}
