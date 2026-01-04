8b3999d39a761017c74637b12d72e69ab68ade63
koeyhnujeel <fksfks015@naver.com>
2026-01-04 16:18:31 +0900

    feat: 도메인 이벤트를 위한 Spring Data Core 의존성 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation("org.springframework:spring-context")
     implementation("org.springframework:spring-tx")
     implementation("org.hibernate.orm:hibernate-core")
+    implementation("org.springframework.data:spring-data-commons:3.5.7")
 }
 
 tasks.named("bootJar") {
