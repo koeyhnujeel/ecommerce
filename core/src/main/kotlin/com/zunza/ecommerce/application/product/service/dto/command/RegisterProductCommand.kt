c2042ac560ee3fa92c8dc4eb603766467184f093
koeyhnujeel <fksfks015@naver.com>
2026-01-08 11:52:29 +0900

    chore

--- core/src/main/kotlin/com/zunza/ecommerce/application/product/service/dto/command/RegisterProductCommand.kt
+++ core/src/main/kotlin/com/zunza/ecommerce/application/product/service/dto/command/RegisterProductCommand.kt
@@ -5,50 +5,50 @@ import com.zunza.ecommerce.domain.product.dto.RegisterProductOptionSpec
 import com.zunza.ecommerce.domain.product.dto.RegisterProductSpec
 import java.math.BigDecimal
 
-    data class RegisterProductCommand(
-        val brandId: Long,
-        val categoryIds: List<Long>,
-        val name: String,
-        val description: String,
-        val basePrice: BigDecimal,
-        val images: List<RegisterProductImageCommand>,
-        val options: List<RegisterProductOptionCommand>
-    ) {
-        fun toSpec() =
-            RegisterProductSpec(
-                brandId = brandId,
-                categoryIds = categoryIds,
-                name = name,
-                description = description,
-                basePrice = basePrice,
-                images = images.map { it.toSpec() },
-                options = options.map { it.toSpec() }
-            )
-    }
+data class RegisterProductCommand(
+    val brandId: Long,
+    val categoryIds: List<Long>,
+    val name: String,
+    val description: String,
+    val basePrice: BigDecimal,
+    val images: List<RegisterProductImageCommand>,
+    val options: List<RegisterProductOptionCommand>
+) {
+    fun toSpec() =
+        RegisterProductSpec(
+            brandId = brandId,
+            categoryIds = categoryIds,
+            name = name,
+            description = description,
+            basePrice = basePrice,
+            images = images.map { it.toSpec() },
+            options = options.map { it.toSpec() }
+        )
+}
 
-    data class RegisterProductImageCommand(
-        val url: String,
-        val type: String,
-        val displayOrder: Int
-    ) {
-        fun toSpec() =
-            RegisterProductImageSpec(
-                url = url,
-                type = type,
-                displayOrder = displayOrder,
-            )
-    }
+data class RegisterProductImageCommand(
+    val url: String,
+    val type: String,
+    val displayOrder: Int
+) {
+    fun toSpec() =
+        RegisterProductImageSpec(
+            url = url,
+            type = type,
+            displayOrder = displayOrder,
+        )
+}
 
-    data class RegisterProductOptionCommand(
-        val color: String,
-        val size: String,
-        val additionalPrice: BigDecimal,
-        val quantity: Int
-    ) {
-        fun toSpec() =
-            RegisterProductOptionSpec(
-                color = color,
-                size = size,
-                additionalPrice = additionalPrice,
-            )
-    }
+data class RegisterProductOptionCommand(
+    val color: String,
+    val size: String,
+    val additionalPrice: BigDecimal,
+    val quantity: Int
+) {
+    fun toSpec() =
+        RegisterProductOptionSpec(
+            color = color,
+            size = size,
+            additionalPrice = additionalPrice,
+        )
+}
